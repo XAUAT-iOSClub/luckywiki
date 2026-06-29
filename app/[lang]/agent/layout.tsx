c7c870ad168e61214dd468237528360e90d134e2
LuckyFish at macOS <2383690354@qq.com>
2026-06-29 22:47:48 +0800

    123

--- app/[lang]/agent/layout.tsx
+++ app/[lang]/agent/layout.tsx
@@ -1,5 +1,7 @@
 import { WikiShell } from "@/components/wiki-shell";
 
+export const dynamic = "force-dynamic";
+
 type Params = Promise<{ lang: string }>;
 
 export default async function AgentLayout({
